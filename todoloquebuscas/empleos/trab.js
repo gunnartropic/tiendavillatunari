e4f0c98d75d70e699ae61bfe83b393c1bca1adbe
gunnar <digitaltropic66@gmail.com>
2024-10-12 17:49:11 -0400

    Actualización de anuncios y tipos desde la base de datos

--- todoloquebuscas/empleos/trab.js
+++ todoloquebuscas/empleos/trab.js
@@ -97,4 +97,98 @@ document.getElementById('albaÃ±il-count').textContent = `(${albaÃ±ilJobs})`;
     if (linkPath === currentPath) {
       link.classList.add('active');
     }
+  });// aquí se agregan los nuevos tipos
+const Servicios_profecionalesJobs = document.querySelectorAll('.job.Servicios_profecionales').length;
+document.getElementById('Servicios_profecionales-count').textContent = `(${Servicios_profecionalesJobs})`;
+const albañilJobs = document.querySelectorAll('.job.albañil').length;
+document.getElementById('albañil-count').textContent = `(${albañilJobs})`;
+const ElectricistaJobs = document.querySelectorAll('.job.Electricista').length;
+document.getElementById('Electricista-count').textContent = `(${ElectricistaJobs})`;
+// fin de los nuevos tipos
+  }
+  
+  function filterJobs(type) {
+    const jobs = document.querySelectorAll('.job');
+    jobs.forEach(job => {
+      if (type === 'all') {
+        job.style.display = 'block';
+      } else {
+        job.style.display = job.classList.contains(type) ? 'block' : 'none';
+      }
+    });
+      
+        if (window.innerWidth <= 768) {
+          document.querySelector('.filter-container').style.display = 'none';
+          document.querySelector('.filter-toggle').style.display = 'block';
+      }
+    }
+  
+  
+     document.querySelector('.filter-toggle').addEventListener('click', function() {
+      const filterContainer = document.querySelector('.filter-container');
+      if (filterContainer.style.display === 'block') {
+          filterContainer.style.display = 'none';
+      } else {
+          filterContainer.style.display = 'block';
+      }
+  });
+  
+ 
+  window.addEventListener('resize', function() {
+      const filterContainer = document.querySelector('.filter-container');
+      const filterToggle = document.querySelector('.filter-toggle');
+      if (window.innerWidth > 768) {
+          filterContainer.style.display = 'block';
+          filterToggle.style.display = 'none';
+      } else {
+          filterContainer.style.display = 'none';
+          filterToggle.style.display = 'block';
+      }
+  });
+  
+  
+  window.addEventListener('load', function() {
+      const filterContainer = document.querySelector('.filter-container');
+      const filterToggle = document.querySelector('.filter-toggle');
+      if (window.innerWidth > 768) {
+          filterContainer.style.display = 'block';
+          filterToggle.style.display = 'none';
+      } else {
+          filterContainer.style.display = 'none';
+          filterToggle.style.display = 'block';
+      }
+  });
+
+  //header botom
+  const checkbox = document.getElementById('btn-menu');
+  const headerMidContainer = document.querySelector('.header-mid-container');
+  const menuIcon = document.querySelector('.menu-icon');
+  
+  document.addEventListener('click', function(event) {
+      // Si haces click fuera del contenedor o en el botÃ³n de menÃº
+      if (!headerMidContainer.contains(event.target) && !checkbox.contains(event.target)) {
+          checkbox.checked = false; // Repliega el menÃº
+      }
+  });
+  
+  menuIcon.addEventListener('click', function(event) {
+      // Si el menÃº estÃ¡ desplegado (checkbox marcado)
+      if (checkbox.checked) {
+          checkbox.checked = false; // Repliega el menÃº
+          event.preventDefault(); // Evita que el checkbox se vuelva a marcar
+      }
+  });
+  
+    /// botom base line
+  const currentPath = window.location.pathname.split('/').pop();
+
+  // Obtener todos los enlaces con la clase 'header-mid'
+  const navLinks = document.querySelectorAll('.header-mid');
+
+  // Iterar sobre los enlaces y comparar la ruta href con la ruta actual
+  navLinks.forEach(link => {
+    const linkPath = link.getAttribute('href').split('/').pop();
+    if (linkPath === currentPath) {
+      link.classList.add('active');
+    }
   });
