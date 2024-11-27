a67bafd169191636a3d5f756a211b02e8149999a
gunnar <digitaltropic66@gmail.com>
2024-11-27 09:26:32 -0400

    Actualización de anuncios y tipos desde la base de datos

--- todoloquebuscas/varios/trab.js
+++ todoloquebuscas/varios/trab.js
@@ -6,12 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const allJobs = document.querySelectorAll('.job').length;
     document.getElementById('all-count').textContent = `(${allJobs})`;
      // aquí se agregan los nuevos tipos
-const Citas_a_ciegasJobs = document.querySelectorAll('.job.Citas_a_ciegas').length;
-document.getElementById('Citas_a_ciegas-count').textContent = `(${Citas_a_ciegasJobs})`;
-const PrestamosJobs = document.querySelectorAll('.job.Prestamos').length;
-document.getElementById('Prestamos-count').textContent = `(${PrestamosJobs})`;
-const Servicios_de_compañíaJobs = document.querySelectorAll('.job.Servicios_de_compañía').length;
-document.getElementById('Servicios_de_compañía-count').textContent = `(${Servicios_de_compañíaJobs})`;
+const freelanceJobs = document.querySelectorAll('.job.freelance').length;
+document.getElementById('freelance-count').textContent = `(${freelanceJobs})`;
 // fin de los nuevos tipos
   }
   
